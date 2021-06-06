5feb20d8c07a34858e72347155c52acad0be276d
logica0419 <nagami.t.aa@m.titech.ac.jp>
2021-06-06 12:14:09 +0900

    Counterを実装

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>{{ countMessage }}</div>
+    <button @click="count++">クリック！</button>
+    <button @click="count = 0">リセット！</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  name: "Counter",
+  setup() {
+    const count = ref(0);
+    const countMessage = computed(() => "回数: " + count.value);
+    return { count, countMessage };
+  },
+};
+</script>
+
+<style></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,22 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>
+    <h1>{{ msg }}</h1>
+    <counter />
+  </div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import Counter from "./Counter.vue";
 
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+export default {
+  components: {
+    Counter,
+  },
+  props: {
+    msg: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
