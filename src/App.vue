6bddfb700256faa9a5fdaf9f0de2b88ac9ca38f6
logica <84759675+logica0419@users.noreply.github.com>
2021-09-04 03:15:03 +0900

    とりあえずts化

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <to-do-list />
+  <to-do-list-online></to-do-list-online>
 </template>
 
-<script>
-import ToDoList from "./components/ToDoList.vue";
+<script lang="ts">
+  import { defineComponent } from "@vue/runtime-core";
+  import ToDoListOnline from "./components/ToDoList Online.vue";
 
-export default {
-  components: {
-    ToDoList,
-  },
-};
+  export default defineComponent({
+    name: "App",
+    components: { ToDoListOnline },
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
